269b8a43295f68ba779f4a69f63739268ae08824
Paolo <tlz98x@gmail.com>
2018-06-12 00:57:01 -0400

    add delete

--- abb.py
+++ abb.py
@@ -4,7 +4,7 @@ class Node:
         self.right = None
         self.data = data
         self.papi = None 
-class BST:
+class abb:
     def __init__(self):
         self.root = None
 
@@ -42,3 +42,58 @@ class BST:
             return self._find(value, node.left)
         elif value > node.value and node.right != none:
             return self._find(value, node.right)
+            def find(self, value):
+        if self.empty():
+            return None
+        else:
+            return self._find(value, self.root)
+
+    def delete(self, value): #Implementar
+        if self.empty():
+            return None
+        return self.delete_node(self.find(value))
+
+    def delete_node(self, node):
+        def min_value_node(n):
+            current = n
+            while current.left != None:
+                current = current.left
+            return current
+        def number_children(n): # Return the number of childrens of the node to be deleted
+            number_children = 0
+            if n.left != None:
+                number_children += 1
+            if n.right != None:
+                number_children += 1
+            return number_children
+
+        node_parent = node.parent # Get the parent of the node to be deleted
+        node_children = number_children(node)
+
+        # Case 1: Deleting a node without childrens
+        if node_children == 0:
+            if node_parent.left == node:
+                node_parent.left = None
+            else:
+                node_parent.right = None
+        # Case 2: Deleting a node with one children
+        if node_children == 1:
+            # Get the children of the node to be deleted
+            if node.left != None:
+                child = node.left
+            else:
+                child = node.right
+
+            # Replace the node to be deleted with its child
+            if node_parent.left == node:
+                node_parent.left = child
+            else:
+                node_parent.right = child
+
+            # Change the parent of the child
+            child.parent = node_parent
+        # Case 3: Deleting a node with two childrens
+        if node_children == 2:
+            successor = min_value_node(node.right) # Get the inorder successor of the deleted node
+            node.value = successor.value # Copy the value
+            self.delete_node(successor)
