07a8b17ac478beccfa35134cbdc9bfdf12085e21
bishuu1 <tlz98x@gmail.com>
2018-07-02 02:30:09 -0400

    add ultimos archivos

--- arbol23.py
+++ arbol23.py
@@ -1,17 +1,102 @@
-class Node:
-    def_init_(self, value)
-        self.data=[value]
-        self.parent= None
-        self.childrens =[]
-class arbol23:
-    def _insert(self, new_node)
-        if len(self.childrens)== 0;
-            self._add(new_node)
-        elif new_node.data[0]> self.data[-1]:
-                self.childrens[-1]._insert(new_node)
-        else
-            for i in range(0, len(self.data))
-                if new_node.data[0]< self.data[i]:
-                    self.childrens[i]._insert(new_node)
-                        break
+class node:
+    def  __init__ ( self , nombre, apellido, telefono, mail ):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.mail = mail
+        self.hijo = []
+        self.padre =  None
+
+ class Arbol:
+    def  __init__ (self):
+        self.raiz = None
+
+    def hoja( self ):
+        return  len(self.hijo) ==  0
+    
+    def  agregar ( self , nombre, apellido, telefono, mail ):
+        for hijo in node(nombre, apellido, telefono, mail).hijo:
+            hijo.padre =  self
+        self.hijo.extend (node(nombre, apellido, telefono, mail).child)
+       
+        if len (self.hijo) > 1 :
+        	self.hijo.sort ()
+        if len (self.data) >  2 :
+            self.Dividir ()
+
+    def Insertar ( self ,nombre, apellido, telefono, mail ):
+        if  self.hoja ():
+            self.agregar(node(nombre, apellido, telefono, mail))
+        elif node(nombre, apellido, telefono, mail).data [ 0 ] >  self.data [ - 1 ]:
+            self.hijo [- 1].Insertar(node(nombre, apellido, telefono, mail))
+        else :
+            for i in range ( 0 , len ( self.data)):
+                if node(nombre, apellido, telefono, mail).data [ 0 ] < self .data [i]:
+                    self .child [i].Insertar(node(nombre, apellido, telefono, mail))
+                    break
+    
+    def Dividir ( self ):
+        HijoIzq = Node ( self.data[0], self )
+        HijoDer = Node ( self.data[2], self )
+            if self.hijo:
+        	self .hijo [ 0 ] .padre = HijoIzq
+        	self .hijo [ 1 ] .padre = HijoIzq
+        	self .hijo [ 2 ] .padre = HijoDer
+        	self .hijo [ 3 ] .padre = HijoDer
+        	HijoIzq.hijo = [self.hijo[0], self.hijo[1]]
+            HijoDer.hijo = [self.hijo[2], self.hijo[3]]
+
+        self.hijo = [HijoIzq]
+        self.hijo.append (HijoDer)
+        self.data = [self.data[1]]
+
+        if self.padre:
+        	if self in self.padre.hijo:
+        		self.padre.hijo._remove(self)
+        	self.padre._add(self)
+        else :
+        	HijoIzq.padre = self
+            HijoDer.padre = self
+
+    def  buscar( self, elemento ):
+    	if elemento in self.data
+    		return elemento
+    	elif  self.hoja():
+    		return False
+    	elif elemento>self.data [-1]:
+    		return  self.hijo[-1].buscar(elemento)
+
+    	else :
+    		for i in range(len(self.data)):
+                if elemento < self .data [i]:
+    				return self.hijo[i]._find(elemento)
+
+    def  eliminar( self, elemento ):
+    	pasar
+
+    def  EnOrden(self):
+    	print(self)
+    	for hijo in self.hijo
+            child._EnOrden()
+
+   
+   def  Vacio (self):
+        return  self.raiz ==  None
+
+        def  insertar ( self , valor ):
+        if  self .Vacio():
+            self.raiz= Node (valor)
+        else :
+            self .raiz._insertar(Node(valor))
+            while  self.raiz.padre:
+                self.raiz = self.raiz.padre
+                    return True
+
+    def  remove ( auto , elemento ):
+        pasar
+
+    def  find ( auto , elemento ):
+        return  self.root._encontrar(elemento)
+
+    
  
--- script.py
+++ script.py
@@ -1,60 +1,63 @@
-import Time
-import Faker
 import ListaDoble
 import abb
 import avl
-import arbol23
 import hashh
 
-def elegir (self,eleccion):
-    tipo = input ("Elija un numero según el tipo de estructur")
+def __main__ ():
+    tipo = input ("Elija un numero segun el tipo de estructur")
     print("1. Lista doble\n 2.abb \n 3.AVL \n 4.2-3Tree \n 5.Hash \n")
     if tipo == 1:
         eleccion = input("que desea hacer?")
-        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda")
+        print("1.Agregar contact \n 2.Eliminar contacto \n 3.Imprimir agenda \n")
+        l= ListaDoble()
         if eleccion == 1:
           nombre = input ("ingrese los datos del contacto separados por un espacio")
-          ListaDoble.insert(nombre)
+          l.insert(nombre)
         elif eleccion == 2:
             lastname == input ("ingrese apellido del contacto a eliminar")    
-            ListaDoble.eliminashon (lastname)
+            l.eliminashon (lastname)
         elif eleccion == 3:
-            ListaDoble.imprimir()
+            l.imprimir()
     if tipo == 2:
-         eleccion = input("que desea hacer?")
-        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda")
+        eleccion = input("que desea hacer?")
+        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda \n")
+        ab = abb()
         if eleccion == 1:
           nombre = input ("ingrese los datos del contacto separados por un espacio")
-          abb.insert(nombre)
+          ab.insert(nombre)
         elif eleccion == 2:
             lastname == input ("ingrese apellido del contacto a eliminar")    
-            abb.delete(lastname)
+            ab.delete(lastname)
         elif eleccion == 3:
-            abb.imprimir_in_order()
+            ab.imprimir_in_order()
 
     if tipo == 3:
-         eleccion = input("que desea hacer?")
-        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda")
+        eleccion = input("que desea hacer?")
+        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda \n")
+        av = avl()
         if eleccion == 1:
           nombre = input ("ingrese los datos del contacto separados por un espacio")
-          avl.insertar(nombre)
+          av.insertar(nombre)
         elif eleccion == 2:
             lastname == input ("ingrese apellido del contacto a eliminar")    
-            avl.eliminar(lastname)
+            av.eliminar(lastname)
         elif eleccion == 3:
-            avl.imprimir_in_order()
-    
+            av.imprimir_in_order()
+    if tipo == 4:
+        print ("Opcion en mantenimiento, por favor elige otra opcion")
+        elegir()
 
     if tipo == 5:
         eleccion = input("que desea hacer?")
-        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda")
+        print("1.agregar contacto \n 2.Eliminar contacto \n 3.Imprimir agenda \n")
+        o = hashh()
         if eleccion == 1:
           nombre = input ("ingrese los datos del contacto separados por un espacio")
-          hashh.insert(nombre)
+          o.insert(nombre)
         elif eleccion == 2:
             lastname == input ("ingrese apellido del contacto a eliminar")    
-            hashh.eliminar(lastname)
+            o.eliminar(lastname)
         elif eleccion == 3:
-            hashh.imprimir_in_order()
+            o.imprimir_in_order()
 
 
